17be7c4c088166e9dca91a2fecafce04cd228042
Thomas Keel <sndbytgkv1@verizon.net>
2017-10-09 23:13:32 -0400

    add break related pgms

---
+++ while_loop_v2_with_break.cpp
@@ -0,0 +1,55 @@
+// while loop with break example
+// from website: https://www.tutorialspoint.com/cplusplus/cpp_break_statement.htm
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Local variable declaration:
+	int a = 10;
+
+	// do loop execution
+	do {
+		cout << "value of a: " << a << endl;
+		a = a + 1;
+		if (a > 15) {
+			// terminate the loop
+			//break;
+		}
+	} while (a < 20);
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+/*  while loop w/o break
+
+value of a : 10
+value of a : 11
+value of a : 12
+value of a : 13
+value of a : 14
+value of a : 15
+
+Press any key to continue . . .
+
+*/
+
+
+/*  while loop w/ break
+
+value of a: 10
+value of a: 11
+value of a: 12
+value of a: 13
+value of a: 14
+value of a: 15
+
+Press any key to continue . . .
+
+*/
